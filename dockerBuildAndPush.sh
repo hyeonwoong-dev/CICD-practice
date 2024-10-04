61fb39dc77b420ff968abd7796bafc6af20cbfae
woomurf <bearoo.work@gmail.com>
2024-10-04 22:51:57 +0900

    initialize

---
+++ dockerBuildAndPush.sh
@@ -0,0 +1,23 @@
+# 모든 서비스 도커 이미지를 빌드합니다.
+services=("server" "company" "gateway" "order" "product" "user")
+
+# 도커 이미지에 commit hash를 기반으로한 이미지 태그를 설정합니다.
+commit_hash=$(git rev-parse --short HEAD)
+
+# AWS ECR 연결
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+for service in "${services[@]}"
+do
+  imageName="sparta-cicd-$service"
+  # 이미지를 구분하기 위해서 latest 이외의 태그를 추가합니다.
+  docker tag "$imageName:latest" "$imageName:$commit_hash"
+
+  # AWS ECR에 Push
+  docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY/$imageName:latest"
+  docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY/$imageName:$commit_hash"
+
+  echo "$service image is built and pushed to AWS ECR"
+done
+
+echo "Build and Push processing is done"
---
+++ order/src/test/java/com/cicd/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
