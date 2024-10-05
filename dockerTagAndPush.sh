65781ace1f112d6f8602a36c0bff117b1d22cf89
woomurf <bearoo.work@gmail.com>
2024-10-06 02:26:59 +0900

    Remove e flag zz

--- dockerTagAndPush.sh
+++ dockerTagAndPush.sh
@@ -1,5 +1,5 @@
 # 모든 서비스 도커 이미지를 빌드합니다.
-services=("server" "gateway" "user") # company, product, order 는 free tier 이슈로 잠시 제외
+services=("server" "gateway" "user")
 
 # 도커 이미지에 commit hash를 기반으로한 이미지 태그를 설정합니다.
 commit_hash=$(git rev-parse --short HEAD)
@@ -8,8 +8,7 @@ for service in "${services[@]}"
 do
   imageName="$ECR_REGISTRY/$ECR_NAMESPACE/$service"
   # 이미지를 구분하기 위해서 latest 이외의 태그를 추가합니다.
-  docker tag "$service:latest" "$imageName:latest"
-  docker tag "$service:latest" "$imageName:$commit_hash"
+  docker tag "$imageName:latest" "$imageName:$commit_hash"
 
   # AWS ECR에 Push
   docker push "$imageName:latest"
